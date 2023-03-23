6f01e345f3f4502949181739f291fc004a4f2d24
Pavel Bocharov <skidysch@mail.ru>
2023-03-23 22:45:06 +0400

    changed secret key and stored in environment.

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('menu.urls')),
+    path('menu/', include('menu.urls')),
 ]
 
 if settings.DEBUG:
